9ddddbc38060b60f89a774773dfe81c2ea819c13
Sansan1980 <152459976+Sansan1980@users.noreply.github.com>
2023-12-18 18:50:03 +0300

    Дз 6/1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        /*
+        Задача 1
+С помощью цикла for выведите в консоль все числа от 1 до 10.
+Критерии оценки
+ Использовали цикл for.
+ Все значения от 1 до 10 вывели в консоль.
+ Переменная в цикле носит простое значение, например буквенное i.
+         */
+        System.out.println("Задача № 1");
+
+        for (int i = 1; i <= 10; i++)
+            System.out.println(i);
+
     }
+
 }
